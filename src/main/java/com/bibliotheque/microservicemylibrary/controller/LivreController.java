14ca8daadc4e4f903a985e99ac0ab87a47a091a5
vinsbld <vincent.blndel@gmail.com>
2020-03-02 17:37:34 +0100

    refreshLivreController

--- src/main/java/com/bibliotheque/microservicemylibrary/controller/LivreController.java
+++ src/main/java/com/bibliotheque/microservicemylibrary/controller/LivreController.java
@@ -1,16 +1,13 @@
 package com.bibliotheque.microservicemylibrary.controller;
 
-import com.bibliotheque.microservicemylibrary.dao.CopieDao;
 import com.bibliotheque.microservicemylibrary.dao.LivreDao;
 import com.bibliotheque.microservicemylibrary.model.Livre;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 import java.util.Optional;
@@ -23,10 +20,7 @@ public class LivreController {
     @Autowired
     private LivreDao livreDao;
 
-    @Autowired
-    private CopieDao copieDao;
-
-    @GetMapping("/Livres")
+    @GetMapping("/livres")
     public List<Livre> ListeDeLivres(){
 
         return livreDao.findAll();
